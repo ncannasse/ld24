2a10af0879c0d48c0e3190b0560d42ffbadad09c
Nicolas Cannasse <ncannasse@motion-twin.com>
2012-08-27 03:53:18 +0200

    added helper NPC

--- src/Hero.hx
+++ src/Hero.hx
@@ -54,6 +54,9 @@ class Hero extends Entity {
 		case "57/38":
 			Sounds.play("npc");
 			game.popup("If you talk to the princess, that will be game ending", "I am married as well, I know what I'm talking about !", true);
+		case "41/72":
+			Sounds.play("npc");
+			game.popup("If you can't find your way, try to push some rock to open the path", "Yes I know, this is quite a classic trick...", true);
 		default:
 			trace("Unknown NPC @" + [n.x, n.y]);
 		}
